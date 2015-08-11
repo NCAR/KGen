7a45178ac4804699aac27f5e38398117b2c67297
amsi7193 <amsi7193@colorado.edu>
2015-08-11 10:36:24 -0600

    Trial 2

--- Test/newtest/src/test_top.F90
+++
@@ -1,7 +0,0 @@
-program test_top
-
-        USE calling_module
-
-        call calling_sub
-
-end program test_top
--- Test/unit_tests/test_7/src/test7.F90
+++
@@ -1,8 +0,0 @@
-program test7
-
-        USE test7_mod
-
-        call test_7
-
-
-end program
