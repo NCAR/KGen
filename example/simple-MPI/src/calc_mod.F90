8c97ca4b1ffd088a603756eb12f98619c274cfcb
Youngsung Kim <youngsun@ucar.edu>
2016-04-26 09:46:10 -0600

    updated examples to apply FC to Makefile in src folders

--- example/simple-MPI/src/calc_mod.F90
+++ example/simple-MPI/src/calc_mod.F90
@@ -4,12 +4,6 @@ CONTAINS
     SUBROUTINE calc(i, j, output)
         INTEGER, INTENT(IN) :: i, j
         INTEGER, INTENT(OUT), dimension(:,:) :: output
-        CALL print_msg('start') 
         output(i,j) = i + j
-        CALL print_msg('finish') 
-    END SUBROUTINE
-    SUBROUTINE print_msg(msg)
-        CHARACTER(*), INTENT(IN) :: msg
-        print *, msg
     END SUBROUTINE
 END MODULE
