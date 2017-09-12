06ebcd3b6bfa2c3274a559d8b26350bcc24ed545
Youngsung Kim <youngsun@ucar.edu>
2017-09-12 13:24:22 -0600

    added remove_state test case

---
+++ test/kapp/func/ys/remove_state/kernel.F90
@@ -0,0 +1,30 @@
+module kernel
+
+        USE types_mod
+
+        implicit none
+
+        !integer(kind=4), parameter :: np =4
+        integer(kind=4), parameter :: niter =10
+
+        public add
+
+        contains
+
+        subroutine add(t1)
+
+                type(complex_type), intent(inout), target :: t1(:)
+                integer :: i
+
+                do i=1,niter
+                        t1(i)%elem1%c(:,:) = (t1(i)%elem2%level3_1%param1(:,:) + &
+                                             t1(i)%elem2%level3_1%param2(:,:)) * &
+                                             (t1(i)%elem1%a(:,:) + &
+                                             t1(i)%elem1%b(:,:))
+                end do
+
+                !print *, "t1(1)%elem1%c(:,:) = ", t1(1)%elem1%c(:,:)
+
+        end subroutine add
+
+end module
